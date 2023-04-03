e458e7a653ed74b05ff9b34cd321cecc899a7e14
Necdet Yavuz AKTAŞ <necdet@Necdet-MacBook-Air.local>
2023-04-03 19:52:30 +0300

    Icons added.

--- lib/product/enums/image_constants.dart
+++ lib/product/enums/image_constants.dart
@@ -1,7 +1,11 @@
 enum IconConstants {
-  like('ic_like');
+  ellipse1('ic_ellipse1'),
+  ellipse2('ic_ellipse2'),
+  ellipse3('ic_ellipse3'),
+  back('ic_back');
 
   final String value;
+  // ignore: sort_constructors_first
   const IconConstants(this.value);
 
   String get toPng => 'assets/icon/$value.png';
