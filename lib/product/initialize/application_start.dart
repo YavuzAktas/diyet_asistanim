15fb400dad19982e12f9c5c5eb75b1ed0e96c2e5
Necdet Yavuz AKTAŞ <necdet@Necdet-MacBook-Air.local>
2023-04-06 15:07:57 +0300

    Login Screen created

--- lib/product/initialize/application_start.dart
+++ lib/product/initialize/application_start.dart
@@ -1,5 +1,8 @@
 import 'package:diyet_asistanim/firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart' show GoogleProvider;
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:firebase_ui_oauth_google/firebase_ui_oauth_google.dart';
 import 'package:flutter/material.dart';
 import 'package:kartal/kartal.dart';
 
@@ -12,5 +15,10 @@ class ApplicationStart {
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
     );
+
+    FirebaseUIAuth.configureProviders([
+      EmailAuthProvider(),
+      GoogleProvider(clientId: ''),
+    ]);
   }
 }
